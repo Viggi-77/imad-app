e4425975aa63c755fe21d45a1c2b3989f2852c10
Viggi-77 <30720778+Viggi-77@users.noreply.github.com>
2017-08-14 01:28:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,12 @@ var element= document.getElementById('main-text');
 element.innerHTML='New Value';
 
 //Move the image
-var img= document.getElementById('madi')
+var img= document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img:style.marginLeft = marginLeft + "px";
+}
+img.onclick = function(){
+    var Interval= setInterval(moveRight,100);
+}
