e4de4e932513d15a86a7dd5256ae0e48ae29c7fd
Viggi-77 <30720778+Viggi-77@users.noreply.github.com>
2017-08-15 01:20:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 var names=[];
-app.get('/submit-one/:name', function(req, res){
+app.get('/submit-name/:name', function(req, res){
    var name=req.params.name; 
    names.push(name);
    //JSON:Javascript Object Notation;
