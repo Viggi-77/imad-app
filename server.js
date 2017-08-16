aa88c67d5901decbe819ef5de284e303d469eac4
Viggi-77 <30720778+Viggi-77@users.noreply.github.com>
2017-08-17 00:11:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,11 +110,23 @@ app.get('/test-db',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     //articleName==articleOne
     //articles[articleName]== content {} of article one
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+   pool.query("SELECT * from article WHERE title '" + req.params.articleName +"'",function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else {
+           if(result.rows.length===0){
+               res.status(404).send('Article is not found');
+          } else {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articles[articleData]));
+            
+               }
+       }
+   });
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
