f21d64f989dc1128f5ec427cb21c1738ae522326
Viggi-77 <30720778+Viggi-77@users.noreply.github.com>
2017-08-30 01:40:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,19 @@ app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
+app.get('/create-user', function(req,res){
+    var salt=crypto.getRandombytes(128).toString('hex');
+    var dbString=hash(password, salt);
+    pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else {
+           res.send('User is created successfully:' +username);
+       }
+    });
+    
+});
 app.get('/articles/:articleName', function(req, res){
     //articleName==articleOne
     //articles[articleName]== content {} of article one
