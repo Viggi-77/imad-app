c63b8ed78dafec2587144cd1f4364a12a1dc98fc
Viggi-77 <30720778+Viggi-77@users.noreply.github.com>
2017-08-30 02:56:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,4 @@
-//Counter Code
-var button=document.getElementById('counter');
-button.onclick= function(){
-    //Make a request to the counter Endpoint
-    var request= new XMLHttpRequest();
-    //Capture response received and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //Take some action
-        if (request.status===200){
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
-            }
-        }
-        //Not Done yet
-    };
-    //Make the request
-    request.open('GET', 'http://hegdevignesh711.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-//Submit name
+//Submit Username/Password to login
 var submit=document.getElementById('submit_btn');
 submit.onclick= function(){
     //Make a request to the server and send the names
@@ -42,10 +21,10 @@ submit.onclick= function(){
         //Not Done yet
     };
     //Make the request
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET', 'http://hegdevignesh711.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST', 'http://hegdevignesh711.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(JSON.stringify({username:username,password:password}));
     
     //Capture a list of the names and render it as a list
    
